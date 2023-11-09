4bc309f3672a5e85a63e541a8de12a9f34219f38
Banuprakash <banuprakashc@yahoo.co.in>
2023-11-09 14:29:33 +0530

    mongodb

---
+++ spring-mongo/src/main/java/com/visa/springmongo/document/Product.java
@@ -0,0 +1,37 @@
+package com.visa.springmongo.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Product {
+	
+	@Id
+	private int id;
+	
+	// @Field("product_title") like @Column
+	private String title;
+	
+	private double price;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
